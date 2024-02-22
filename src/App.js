2e326209b6e9dba8560c66ed6111cbeffc47d96a
rilinca <radu-mihai.ilinca@ionos.com>
2024-02-22 19:35:54 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import ChooseProfile from "./Pages/ChooseProfile.tsx";
 import Exercises from "./Pages/Exercises.tsx";
 import Food from "./Pages/Food.tsx";
@@ -8,19 +9,19 @@ import Register from "./Pages/Register.tsx";
 
 
 function App() {
-
-
   return (
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="home" element={<Home />} />
-    <Route path="home/exercises" element={<Exercises />} />
-    <Route path="home/food" element={<Food />} />
-    <Route path="register" element={<Register />} />
-    <Route path="login" element={<Login />} />
-    <Route path="chooseProfile" element={<ChooseProfile />} />
-    <Route path="*" element={<Home />} />
-  </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="home/exercises" element={<Exercises />} />
+        <Route path="home/food" element={<Food />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="chooseProfile" element={<ChooseProfile />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
